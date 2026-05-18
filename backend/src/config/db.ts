69a9099fa0c3e38c8a3af3cbf176ac32c8f2f2da
Sarakshimore <sarakshimoore@gmail.com>
2026-05-19 00:20:40 +0530

    Updated db config file

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,7 +4,7 @@ const connectDB = async (): Promise<void> => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB Connected');
   } catch (error) {
     console.error("Database connection failed");
 
